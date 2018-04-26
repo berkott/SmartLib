83dad35eaec70347e6a151f31e4f02af6aac7f05
berkott <berkott3002@gmail.com>
2018-04-27 05:38:02 +0900

    added some js

---
+++ www/js/checkout.js
@@ -0,0 +1,6 @@
+var storage = firebase.storage();
+var storageRef = storage.ref();
+storageRef.child('map.jpg').getDownloadURL().then(function(url) {
+  var img = document.getElementById('mapPic');
+  img.src = url;
+});
